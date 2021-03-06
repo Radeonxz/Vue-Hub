ca1d2f3a725271cb76aa460d9407818d1526cd49
Radeonxz <xuanxxzhao@gmail.com>
2021-03-06 10:42:42 -0500

    chore: add app styles and item

--- twitTut/src/App.vue
+++ twitTut/src/App.vue
@@ -10,23 +10,23 @@
         {{ user.username }}
       </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { useStore } from "vuex";
+import { computed } from "vue";
 export default {
-  name: 'App',
+  name: "App",
   setup() {
     const store = useStore();
     const user = computed(() => store.state.User.user);
     return {
       user
-    }
+    };
   }
-}
+};
 </script>
 
 <style lang="scss">
@@ -34,8 +34,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  background-color: #f3f5fa;
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 5%;
+    background-color: deeppink;
+    color: white;
+    .navigation__logo {
+      font-weight: bold;
+      font-size: 24px;
+    }
+    .navigation__user {
+      font-weight: bold;
+    }
+  }
 }
 </style>
