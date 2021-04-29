2672d934e64c5c61eaf928b3ca20ede9d67578f0
Radeonxz <xuanxxzhao@gmail.com>
2021-04-29 18:11:09 -0400

    chore: add search

--- apex-tracker/client/src/router.js
+++ apex-tracker/client/src/router.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Search from "./components/Search.vue";
 import Profile from "./components/Profile.vue";
 
 Vue.use(Router);
@@ -7,6 +8,11 @@ Vue.use(Router);
 export default new Router({
   mode: "history",
   routes: [
+    {
+      path: "/",
+      name: "search",
+      component: Search
+    },
     {
       path: "/profile/:platform/:gamertag",
       name: "profile",
